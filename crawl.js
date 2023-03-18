e00d2bde514b86cb0aecae5336c4f5d67fd02822
Ashima Tripathi <ashima@go-gaga-app.com>
2023-03-19 00:43:10 +0530

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,4 +1,26 @@
-const {JSDOM} = require('jsdom')
+const { JSDOM } = require('jsdom')
+
+async function crawlPage(currentURL){
+    console.log(`actively crawling: ${currentURL}`)
+    try {
+        const resp = await fetch(currentURL)
+        if (resp.status > 399) {
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return 
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")) {
+            console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`)
+            return
+        }
+
+        console.log(await resp.text())
+    } catch (err){
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    }
+    
+}
 
 function getURLsFromHTML(htmlBody, baseURL){
     const urls = []
@@ -37,5 +59,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
